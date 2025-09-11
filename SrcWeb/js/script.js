f2b501ae75b3b6bf4460dd9662187265afe9ae58
dev-2332516 <2332516@etu.cchic.ca>
2025-09-11 10:39:53 -0400

    feat: autre version plutot stable !

--- SrcWeb/js/script.js
+++ SrcWeb/js/script.js
@@ -1,8 +1,10 @@
 const gameContainer = document.getElementById('grid-container')
+const posX = 8;
+const posY = 8;
 
-createGrid(5, 5);
+createGrid();
 
-displayDefaultTiles();
+// displayDefaultTiles();
 
 async function GetTile(x, y, td) {
   try {
@@ -18,24 +20,24 @@ async function GetTile(x, y, td) {
   }
 }
 
-function showCoordinates() {
-
+function showCoordinates() { 
+  
 }
 
-async function createGrid(rows, cols) {
+async function createGrid() {
   const table = document.createElement('table');
-  for (let r = 0; r < rows; r++) {
+  for (let r = posY; r < posY + 5; r++) {
     const tr = document.createElement('tr');
-    for (let c = 0; c < cols; c++) {
+    for (let c = posX; c < posX + 5; c++) {
       const td = document.createElement('td');
 
       // Calculer les coordonnées centrées
-      const centeredX = c - Math.floor(cols / 2) + 10;
-      const centeredY = r - Math.floor(rows / 2) + 10;
-      // const response = await fetch(`https://localhost:7223/api/Tuiles/${x}/${y}`);
-      // const tile = await response.json()
-      // const centeredX = tile.positionX;
-      // const centeredY = tile.pos;
+      // const centeredX = c - Math.floor(cols / 2) + 10;
+      // const centeredY = r - Math.floor(rows / 2) + 10;
+      const response = await fetch(`https://localhost:7223/api/Tuiles/${c}/${r}`);
+      const tile = await response.json()
+      const centeredX = tile.positionX;
+      const centeredY = tile.positionY;
       
       td.id = `tile-${centeredX}-${centeredY}`;
 
@@ -53,14 +55,15 @@ async function createGrid(rows, cols) {
   }
 
   gameContainer.appendChild(table);
+  displayDefaultTiles();
 }
 
 async function displayDefaultTiles() {
   for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
-      const td = document.getElementById(`tile-${10 - x}-${10 - y}`);
+      const td = document.getElementById(`tile-${posX + 2 - x}-${posY + 2 - y}`);
       if (td) {
-        await GetTile(10 - x, 10 - y, td);
+        await GetTile(posX + 2 - x, posY + 2 - y, td);
       }
     }
   }
