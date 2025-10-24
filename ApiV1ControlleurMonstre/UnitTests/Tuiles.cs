2161458b48a5897879bdefaf5d579c774c1d94fb
dev-2332516 <2332516@etu.cchic.ca>
2025-10-24 15:51:58 -0400

    feat: reference de projet

--- ApiV1ControlleurMonstre/UnitTests/Tuiles.cs
+++ ApiV1ControlleurMonstre/UnitTests/Tuiles.cs
@@ -1,9 +1,23 @@
-﻿namespace UnitTests
+﻿using ApiV1ControlleurMonstre.Data.DTOs;
+using Xunit;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace UnitTests
 {
-    public class Tuiles
+    public class Tuiles : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public SimpleGameFlowTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
     {
         [Fact]
-        public void Test1()
+        public void Inscription_WithValidData_ReturnsCreated()
         {
 
         }
